3e97c350f0fd106e2f20e08bebcbdec034756247
kerby <kerbysoh@gmail.com>
2023-08-05 19:43:08 +0800

    Setup basic login and registration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom'
+import Dashboard from './pages/dashboard'
+import Login from './pages/login'
+import Register from './pages/register'
+import { useSelector, Provider } from 'react-redux'
+import { store } from './redux/store'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PrivateRoutes = () => {
+  const { isAuth } = useSelector((state) => state.auth)
+
+  return <>{isAuth ? <Outlet /> : <Navigate to='/login' />}</>
 }
 
-export default App;
+const RestrictedRoutes = () => {
+  const { isAuth } = useSelector((state) => state.auth)
+
+  return <>{!isAuth ? <Outlet /> : <Navigate to='/dashboard' />}</>
+}
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Navigate to="/login" />}  />
+          <Route element={<PrivateRoutes />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Route>
+
+          <Route element={<RestrictedRoutes />}>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default App
