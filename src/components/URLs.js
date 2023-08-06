7cc0732e7d19aa1c9fa55156b1c4a779966d4906
kerby <kerbysoh@gmail.com>
2023-08-06 17:49:23 +0800

    Add in production server URL

--- src/components/URLs.js
+++ src/components/URLs.js
@@ -49,7 +49,7 @@ const URLs = (props) => {
         let serverURL = process.env.REACT_APP_SERVER_URL
 
         if (process.env.NODE_ENV === 'production') {
-            serverURL = ""
+            serverURL = "https://shrtn-backend.onrender.com"
         }
 
         const shortUrl = serverURL + `/links/${link.id}`
